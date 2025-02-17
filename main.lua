691ad8249d38314b1ca7b90aa2975cb43956db8b
1-16AM <rt@dx9.pro>
2025-02-16 23:07:21 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+local supportedGames = {
+    [2955322961] = true, -- shrimp game
+    [6942875911] = true, -- squid project
+}
+
+local function loadGame()
+    if not supportedGames[game.GameId] then
+        warn("xena does not support this game yet (universal coming soon)")
+        return
+    end
+
+    local success, error = pcall(function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/1-16AM/xena/refs/heads/main/games/'.. game.GameId ..'.lua'))()
+    end)
+
+    if not success then
+        warn("failed to load script:", error)
+    end
+end
+
+loadGame()
