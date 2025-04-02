0aa954d3bd8fdbf3e683439c5e11e3326bedce48
1-16AM <rt@dx9.pro>
2025-04-02 14:28:22 -0400

    Create dont1v1.lua

---
+++ functions/dont1v1.lua
@@ -0,0 +1,9 @@
+-- stricly people who have locked on me and i need to save to not go against in games, or they have just killed me and ion wanna forget
+
+local people = {
+    ["normalgirIbysza"] = true,
+    ["enpicism"] = true,
+    ["bleh0162"] = true
+}
+
+return people
