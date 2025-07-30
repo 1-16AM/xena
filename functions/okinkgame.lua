7960f5e71e4e0f3b8bfb943fed9e0ca337560f25
1-16AM <rt@dx9.pro>
2025-07-30 16:58:49 -0400

    Update okinkgame.lua

--- functions/okinkgame.lua
+++ functions/okinkgame.lua
@@ -1,44 +1,26 @@
-if not hookfunction then
-	local regui =
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/depthso/Dear-ReGui/refs/heads/main/ReGui.lua"))()
+local regui = loadstring(
+    game:HttpGet(
+        'https://raw.githubusercontent.com/depthso/Dear-ReGui/refs/heads/main/ReGui.lua'
+    )
+)()
 
-	for i=1, 16 do
-		local prompt = regui
-		:Window({
-			Title = "Sorry..",
-			Size = UDim2.fromOffset(300, 125),
-			NoCollapse = true,
-			NoResize = true,
-			NoScroll = true,
-		})
-		:Center()
-	do
-		prompt:Label({
-			Text = "YOUR EXECUTOR DOES NOT SUPPORT HOOKFUNCTION PLZ USE A DIFFERENT SHITSPLOIT",
-			TextWrapped = true
-		})
-		prompt:Separator()
-		prompt:Label({
-			Text = "Kicking you to avoid you getting banned",
-			TextWrapped = true
-		})
-		prompt:Label({
-			Text = "discord.gg/VSKCM7rXVY"
-		})
-	end
-    end
-	task.wait(3)
-	game.Players.LocalPlayer:Kick("Please change your executor; discord.gg/VSKCM7rXVY")
-end
+local prompt = regui
+    :Window({
+        Title = 'Sorry..',
+        Size = UDim2.fromOffset(300, 125),
+        NoCollapse = true,
+        NoResize = true,
+        NoScroll = true,
+    })
+    :Center()
+prompt:Label({
+    Text = 'This script is currently detected, it is being worked on to mitigate the detections. Join the discord for updates (SOON)',
+    TextWrapped = true,
+})
+prompt:Separator()
+prompt:Label({
+    Text = 'discord.gg/VSKCM7rXVY',
+})
 
-local anticheatok = game:GetService("ReplicatedStorage").Remotes[""]
-local blockanticheat
-blockanticheat = hookfunction(anticheatok.FireServer, function(self, ...)
-	if self == anticheatok then
-		print(`Blocked the shit-anti-cheat`)
-		task.wait(9e9)
-		return nil
-	end
-
-	return blockanticheat(self, ...)
-end)
+task.wait(3)
+game.Players.LocalPlayer:Kick("Join the discord to be notified when this is undetected; discord.gg/VSKCM7rXVY")
